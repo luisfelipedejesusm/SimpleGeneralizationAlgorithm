a6700828ef42089c178a996db70c295b126aa06f
luis <luis@DO493PC0KDT36O.intl.bns>
2017-12-01 16:58:16 -0400

    Simple Generalization Algorithm

--- KNN.js
+++ KNN.js
@@ -136,8 +136,8 @@ var interval = null
 		var canvas = document.getElementById(canvas_id);
 		console.log(canvas)
 		var ctx = canvas.getContext("2d");
-		var width = 700;
-		var height = 700;
+		var width = 400;
+		var height = 400;
 		ctx.clearRect(0,0,width, height);
 
 		for (var i in this.nodes)
@@ -146,19 +146,69 @@ var interval = null
 
 			switch (this.nodes[i].type)
 			{
-				case 'apartment':
-					ctx.fillStyle = 'red';
+				case 'turquesa':
+					ctx.fillStyle = '#1abc9c';
 					break;
-				case 'house':
-					ctx.fillStyle = 'green';
+				case 'esmeralda':
+					ctx.fillStyle = '#2ecc71';
 					break;
-				case 'flat':
-					ctx.fillStyle = 'blue';
+				case 'lightblue':
+					ctx.fillStyle = '#3498db';
+					break;
+				case 'lightpurple':
+					ctx.fillStyle = '#9b59b6';
+					break;
+				case 'almostblack':
+					ctx.fillStyle = '#34495e';
+					break;
+				case 'greensea':
+					ctx.fillStyle = '#16a085';
+					break;
+				case 'darkgreen':
+					ctx.fillStyle = '#27ae60';
+					break;
+				case 'belizehole':
+					ctx.fillStyle = '#2980b9';
+					break;
+				case 'darpurple':
+					ctx.fillStyle = '#8e44ad';
+					break;
+				case 'midnigth':
+					ctx.fillStyle = '#2c3e50';
+					break;
+				case 'sunflower':
+					ctx.fillStyle = '#f1c40f';
+					break;
+				case 'carrot':
+					ctx.fillStyle = '#e67e22';
+					break;
+				case 'allizarin':
+					ctx.fillStyle = '#e74c3c';
+					break;
+				case 'clouds':
+					ctx.fillStyle = '#ecf0f1';
+					break;
+				case 'concrete':
+					ctx.fillStyle = '#95a5a6';
+					break;
+				case 'orange':
+					ctx.fillStyle = '#f39c12';
+					break;
+				case 'pumpkin':
+					ctx.fillStyle = '#d35400';
+					break;
+				case 'darred':
+					ctx.fillStyle = '#c0392b';
+					break;
+				case 'silver':
+					ctx.fillStyle = '#bdc3c7';
+					break;
+				case 'darkgray':
+					ctx.fillStyle = '#7f8c8d';
 					break;
 				default:
 					ctx.fillStyle = '#666666';
 			}
-
 			var padding = 40;
 			var x_shift_pct = (width  - padding) / width;
 			var y_shift_pct = (height - padding) / height;
@@ -171,54 +221,93 @@ var interval = null
 			ctx.translate(x, y);
 			ctx.beginPath();
 
-			switch (this.nodes[i].type)
-			{
-				case 'apartment':
+			// switch (this.nodes[i].type)
+			// {
+			// 	case 'turquesa':
 					ctx.arc(0, 0, 5, 0, Math.PI*2, true);
-					break;
-				case 'house':
-					ctx.fillRect(0, 0, 10, 10)
-					break;
-				case 'flat':
-					ctx.moveTo(0, 0);
-					ctx.lineTo(-5, 10);
-					ctx.lineTo(5, 10);
-					break;
-				default:
-					ctx.arc(0, 0, 5, 0, Math.PI*2, true);
-			}
-
-			// ctx.arc(0, 0, 5, 0, Math.PI*2, true);
-			// ctx.fillRect(0, 0, 10, 10)
-
-			// ctx.moveTo(100, 100);
-			// ctx.lineTo(95, 110);
-			// ctx.lineTo(105, 110);
+			// 		break;
+			// 	case 'green':
+			// 		ctx.fillRect(0, 0, 10, 10)
+			// 		break;
+			// 	case 'blue':
+			// 		ctx.moveTo(0, 0);
+			// 		ctx.lineTo(-5, 10);
+			// 		ctx.lineTo(5, 10);
+			// 		break;
+			// 	default:
+			// 		ctx.arc(0, 0, 5, 0, Math.PI*2, true);
+			// }
 
 			ctx.fill();
 			ctx.closePath();
-			
-
-			/* 
-			 * Is this an unknown node? If so, draw the radius of influence
-			 */
 
 			if ( ! this.nodes[i].type )
 			{
-				switch (this.nodes[i].guess.type)
-				{
-					case 'apartment':
-						ctx.strokeStyle = 'red';
-						break;
-					case 'house':
-						ctx.strokeStyle = 'green';
-						break;
-					case 'flat':
-						ctx.strokeStyle = 'blue';
-						break;
-					default:
-						ctx.strokeStyle = '#666666';
-				}
+				switch (this.nodes[i].type)
+			{
+				case 'turquesa':
+					ctx.strokeStyle = '#1abc9c';
+					break;
+				case 'esmeralda':
+					ctx.strokeStyle = '#2ecc71';
+					break;
+				case 'lightblue':
+					ctx.strokeStyle = '#3498db';
+					break;
+				case 'lightpurple':
+					ctx.strokeStyle = '#9b59b6';
+					break;
+				case 'almostblack':
+					ctx.strokeStyle = '#34495e';
+					break;
+				case 'greensea':
+					ctx.strokeStyle = '#16a085';
+					break;
+				case 'darkgreen':
+					ctx.strokeStyle = '#27ae60';
+					break;
+				case 'belizehole':
+					ctx.strokeStyle = '#2980b9';
+					break;
+				case 'darpurple':
+					ctx.strokeStyle = '#8e44ad';
+					break;
+				case 'midnigth':
+					ctx.strokeStyle = '#2c3e50';
+					break;
+				case 'sunflower':
+					ctx.strokeStyle = '#f1c40f';
+					break;
+				case 'carrot':
+					ctx.strokeStyle = '#e67e22';
+					break;
+				case 'allizarin':
+					ctx.strokeStyle = '#e74c3c';
+					break;
+				case 'clouds':
+					ctx.strokeStyle = '#ecf0f1';
+					break;
+				case 'concrete':
+					ctx.strokeStyle = '#95a5a6';
+					break;
+				case 'orange':
+					ctx.strokeStyle = '#f39c12';
+					break;
+				case 'pumpkin':
+					ctx.strokeStyle = '#d35400';
+					break;
+				case 'darred':
+					ctx.strokeStyle = '#c0392b';
+					break;
+				case 'silver':
+					ctx.strokeStyle = '#bdc3c7';
+					break;
+				case 'darkgray':
+					ctx.strokeStyle = '#7f8c8d';
+					break;
+				default:
+					ctx.strokeStyle = '#666666';
+			}
 
 				var radius = this.nodes[i].neighbors[this.k - 1].distance * width;
 				radius *= x_shift_pct;
@@ -250,9 +339,10 @@ var interval = null
 	};
 	var data = []
 	createData = function(){
-		let types = ['house', 'apartment', 'flat']
-		for(var i = 0; i < 3; i++){
-			for(var ct = 0; ct < 5; ct++){
+		let types = ['turquesa','esmeralda',' lightblue','lightpurple','almostblack','greensea','darkgreen','belizehole',
+		'darpurple','midnigth','sunflower','carrot','allizarin','clouds','concrete','orange','pumpkin','darred','silver','darkgray']
+		for(var i = 0; i < 20; i++){
+			for(var ct = 0; ct < 2; ct++){
 				var random_rooms = Math.round( Math.random() * 2000 );
 				var random_area = Math.round( Math.random() * 2000 );
 				data.push({type: types[i], area: random_area, rooms: random_rooms})
